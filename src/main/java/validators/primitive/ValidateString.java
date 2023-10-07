ff6cda6063227514b67b5de460c454bcba2b254a
yura <triplef@disroot.org>
2023-10-07 11:39:45 +0600

    Validator

--- src/main/java/validators/primitive/ValidateString.java
+++ src/main/java/validators/primitive/ValidateString.java
@@ -30,11 +30,22 @@ public class ValidateString {
         return true;
     }
 
-    public boolean validateStringStatus(String str, List<String> errors, String fieldName, String errorMessage) {
-        if (!str.equals("учится") && !str.equals("в а/о") && !str.equals("отчислен")) {
+    public boolean validateStringInt(String id, List<String> errors, String fieldName, String errorMessage) {
+        for (int i = 0; i < id.length(); i++) {
+            if (!Character.isDigit(id.charAt(i))) {
+                errors.add(fieldName + ": " + errorMessage);
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean validateStringStatus(String status, List<String> errors, String fieldName, String errorMessage) {
+        if (!status.equals("учится") && !status.equals("в а/о") && !status.equals("отчислен")) {
             errors.add(fieldName + ": " + errorMessage);
             return false;
         }
         return true;
     }
+
 }
