a3a33342c3c80402d874ad1a8186b0100d5f03a1
yura <triplef@disroot.org>
2023-11-11 10:36:27 +0600

    ControllerGroup3

--- src/main/java/ru_omsu_fctk_simpleserver/controllers/group/ControllerGroup.java
+++ src/main/java/ru_omsu_fctk_simpleserver/controllers/group/ControllerGroup.java
@@ -2,18 +2,21 @@ package ru_omsu_fctk_simpleserver.controllers.group;
 
 import ru_omsu_fctk_simpleserver.controllers.ComonResponse;
 import ru_omsu_fctk_simpleserver.controllers.ResponseEntity;
-import ru_omsu_fctk_simpleserver.request.group.AddStudentGroupsRequest;
-import ru_omsu_fctk_simpleserver.request.group.EditStudentGroupsRequest;
-import ru_omsu_fctk_simpleserver.response.group.AddStudentGroupsResponse;
-import ru_omsu_fctk_simpleserver.response.group.EditStudentGroupsResponse;
+import ru_omsu_fctk_simpleserver.request.group.*;
+import ru_omsu_fctk_simpleserver.response.group.*;
 import ru_omsu_fctk_simpleserver.validator.group.AddStudentGroupsValidator;
+import ru_omsu_fctk_simpleserver.validator.group.DeleteStudentGroupValidator;
 import ru_omsu_fctk_simpleserver.validator.group.EditStudentGroupsValidator;
+import ru_omsu_fctk_simpleserver.validator.group.GetStudentGroupByIdValidator;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ControllerGroup {
     private AddStudentGroupsValidator addStudentGroupsValidator;
     private EditStudentGroupsValidator editStudentGroupsValidator;
+    private DeleteStudentGroupValidator deleteStudentGroupValidator;
+    private GetStudentGroupByIdValidator getStudentGroupByIdValidator;
 
     public ResponseEntity<ComonResponse<AddStudentGroupsResponse>> addStudentGroups(AddStudentGroupsRequest request) {
         int status = 200;
@@ -59,9 +62,62 @@ public class ControllerGroup {
         return new ResponseEntity<>(comonResponse, status);
     }
 
+    public ResponseEntity<ComonResponse<DeleteStudentGroupResponse>> deleteStudentGroup(DeleteStudentGroupRequest request) {
+        int status = 200;
+        ComonResponse<DeleteStudentGroupResponse> comonResponse;
 
+        List<String> errors = deleteStudentGroupValidator.validator(request);
 
+        if (errors.isEmpty()) {
+            try {
+                DeleteStudentGroupService service = new DeleteStudentGroupService(request);
+                comonResponse = new ComonResponse<>(service.answer());
+            } catch (Exception exception) {
+                status = 422;
+                comonResponse = new ComonResponse<>(6, "Error", errors); // своё сообщение об ошибке
+            }
+        } else {
+            status = 400;
+            comonResponse = new ComonResponse<>(6, "Error", errors); // своё сообщение об ошибке
+        }
 
+        return new ResponseEntity<>(comonResponse, status);
+    }
 
+    public ResponseEntity<ComonResponse<GetStudentGroupByIdResponse>> getStudentGroupByIdGroup(GetStudentGroupByIdRequest request) {
+        int status = 200;
+        ComonResponse<GetStudentGroupByIdResponse> comonResponse;
 
+        List<String> errors = getStudentGroupByIdValidator.validator(request);
+
+        if (errors.isEmpty()) {
+            try {
+                GetStudentGroupByIdService service = new GetStudentGroupByIdService(request);
+                comonResponse = new ComonResponse<>(service.answer());
+            } catch (Exception exception) {
+                status = 422;
+                comonResponse = new ComonResponse<>(6, "Error", errors); // своё сообщение об ошибке
+            }
+        } else {
+            status = 400;
+            comonResponse = new ComonResponse<>(6, "Error", errors); // своё сообщение об ошибке
+        }
+
+        return new ResponseEntity<>(comonResponse, status);
+    }
+
+    private ResponseEntity<ComonResponse<GetStudentGroupsResponse>> getStudentGroups(GetStudentGroupsRequest request) {
+        int status = 200;
+        ComonResponse<GetStudentGroupsResponse> comonResponse;
+
+        try {
+            GetStudentGroupsService service = new GetStudentGroupsService(request);
+            comonResponse = new ComonResponse<>(service.answer());
+        } catch (Exception exception) {
+            status = 422;
+            comonResponse = new ComonResponse<>(6, "Error", new ArrayList<>()); // своё сообщение об ошибке
+        }
+
+        return new ResponseEntity<>(comonResponse, status);
+    }
 }
