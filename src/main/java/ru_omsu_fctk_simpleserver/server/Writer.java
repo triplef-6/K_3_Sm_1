94858c214cdd1e1b35abc8ae7edb3639a34b01b6
yura <triplef@disroot.org>
2023-12-04 10:56:08 +0600

    Создан Writer

---
+++ src/main/java/ru_omsu_fctk_simpleserver/server/Writer.java
@@ -0,0 +1,16 @@
+package ru_omsu_fctk_simpleserver.server;
+
+/**
+ * вывод
+ */
+public class Writer {
+    private String response;
+
+    public Writer(String request) {
+        this.response = request;
+    }
+
+    public String getResponse() {
+        return response;
+    }
+}
