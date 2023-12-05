da3263adfa715901c410a4c7caac3a4cc3e7b113
yura <triplef@disroot.org>
2023-12-05 18:04:18 +0600

    Работает для групп

--- src/main/java/MEIN.java
+++ src/main/java/MEIN.java
@@ -7,9 +7,32 @@ public class MEIN {
         Server server = new Server();
 
         try {
-            Reader reader1 = new Reader("addStudentGroups::{\"name\":\"MMB-103\"}");
-            Writer writer1 = server.executeRequest(reader1);
-            System.out.println(writer1);
+            Reader[] readers = {
+                    new Reader("addStudentGroups::{\"name\":\"MMB-101\"}"),
+                    new Reader("addStudentGroups::{\"name\":\"MMB-102\"}"),
+                    new Reader("addStudentGroups::{\"name\":\"MMB-103\"}"),
+                    new Reader("addStudentGroups::{\"name\":\"MMB-104\"}"),
+
+                    new Reader("getStudentGroupById::{\"id\":\"1\"}"),
+                    new Reader("getStudentGroupById::{\"id\":\"2\"}"),
+                    new Reader("getStudentGroupById::{\"id\":\"3\"}"),
+                    new Reader("getStudentGroupById::{\"id\":\"4\"}"),
+
+                    new Reader("editStudentGroups::{\"id\":\"3\",\"name\":\"\"}"),
+                    new Reader("getStudentGroupById::{\"id\":\"3\"}"),
+
+                    new Reader("deleteStudentGroup::{\"id\":\"1\"}"),
+                    new Reader("getStudentGroupById::{\"id\":\"1\"}"),
+
+                    new Reader("deleteStudentGroup::{\"id\":\"2\"}"),
+
+                    new Reader("editStudentGroups:: ")
+            };
+            Writer[] writers = new Writer[readers.length];
+            for (int i = 0; i < readers.length; i++) {
+                writers[i] = server.executeRequest(readers[i]);
+                System.out.println(writers[i]);
+            }
         } catch (Exception e) {
             throw new Exception(e.getMessage());
         }
