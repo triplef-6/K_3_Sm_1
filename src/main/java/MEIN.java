2887293b706c2d66b93a79b0a52edcb66b0bd389
yura <triplef@disroot.org>
2023-12-06 13:08:01 +0600

    Работает для студентов и групп

--- src/main/java/MEIN.java
+++ src/main/java/MEIN.java
@@ -2,6 +2,22 @@ import ru_omsu_fctk_simpleserver.server.Reader;
 import ru_omsu_fctk_simpleserver.server.Server;
 import ru_omsu_fctk_simpleserver.server.Writer;
 
+/**
+ * Group:
+ * addStudentGroups::{\"name\":\"имя группы\"}
+ * editStudentGroups::{\"id\":\"id группы\",\"имя группы\":\"\"}
+ * deleteStudentGroup::{"id":"id группы"}
+ * getStudentGroupById::{"id":"id группы"}
+ * getStudentGroups::
+ *
+ * Student:
+ * addStudent::{\"groupId\":\"id группы\",\"surname\":\"фамилия\",\"name\":\"имя\",\"patronymic\":\"отчество\",\"status\":\"статус\"}
+ * editStudent::{\"id\":\"id студента\",\"groupId\":\"id группы\",\"surname\":\"фамилия\",\"name\":\"имя\",\"patronymic\":\"отчество\",\"status\":\"статус\"}
+ * deleteStudent::{\"id\":\"id студента\"}
+ * getStudentById::{\"id\":\"id студента\"}
+ * getStudentByGroup::{\"id\":\"id группы\"}
+ */
+
 public class MEIN {
     public static void main(String[] args) throws Exception {
         Server server = new Server();
@@ -12,8 +28,27 @@ public class MEIN {
                     new Reader("addStudentGroups::{\"name\":\"MMB-102\"}"),
                     new Reader("addStudentGroups::{\"name\":\"MMB-103\"}"),
                     new Reader("addStudentGroups::{\"name\":\"MMB-104\"}"),
+                    new Reader("getStudentGroups:: "),
 
-                    new Reader("getStudentGroupById::{\"id\":\"1\"}"),
+                    new Reader("addStudent::{\"groupId\":\"1\",\"surname\":\"Носов\",\"name\":\"Никита\",\"patronymic\":\"Отчество\",\"status\":\"учится\"}"),
+                    new Reader("getStudentById::{\"id\":\"1\"}"),
+
+                    new Reader("addStudent::{\"groupId\":\"3\",\"surname\":\"Алёшкин\",\"name\":\"Александр\",\"patronymic\":\"Отчество\",\"status\":\"учится\"}"),
+                    new Reader("getStudentById::{\"id\":\"2\"}"),
+
+                    new Reader("addStudent::{\"groupId\":\"4\",\"surname\":\"Кошевая\",\"name\":\"Анастасия\",\"patronymic\":\"Отчество\",\"status\":\"учится\"}"),
+                    new Reader("getStudentById::{\"id\":\"3\"}"),
+
+                    new Reader("getStudentByGroup::{\"id\":\"1\"}"),
+                    new Reader("getStudentByGroup::{\"id\":\"3\"}"),
+                    new Reader("getStudentByGroup::{\"id\":\"4\"}"),
+
+                    new Reader("editStudent::{\"id\":\"1\",\"groupId\":\"1\",\"surname\":\"Носов\",\"name\":\"Никита\",\"patronymic\":\"Отчество\",\"status\":\"отчислен\"}"),
+                    new Reader("getStudentById::{\"id\":\"1\"}"),
+
+                    new Reader("editStudent::{\"id\":\"2\",\"groupId\":\"4\",\"surname\":\"Алёшкин\",\"name\":\"Александр\",\"patronymic\":\"Отчество\",\"status\":\"учится\"}"),
+                    new Reader("getStudentByGroup::{\"id\":\"4\"}")
+                   /*new Reader("getStudentGroupById::{\"id\":\"1\"}"),
                     new Reader("getStudentGroupById::{\"id\":\"2\"}"),
                     new Reader("getStudentGroupById::{\"id\":\"3\"}"),
                     new Reader("getStudentGroupById::{\"id\":\"4\"}"),
@@ -25,8 +60,11 @@ public class MEIN {
                     new Reader("getStudentGroupById::{\"id\":\"1\"}"),
 
                     new Reader("deleteStudentGroup::{\"id\":\"2\"}"),
+                    new Reader("getStudentGroups:: "),
 
-                    new Reader("editStudentGroups:: ")
+                    new Reader("editStudentGroups::{\"id\":\"3\",\"name\":\"MMB-103-O1\"}"),
+                    new Reader("editStudentGroups::{\"id\":\"4\",\"name\":\"MMB-104-O2\"}"),
+                    new Reader("getStudentGroups:: ")*/
             };
             Writer[] writers = new Writer[readers.length];
             for (int i = 0; i < readers.length; i++) {
