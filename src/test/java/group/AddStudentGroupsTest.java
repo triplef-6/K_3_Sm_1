f830df0f5ad4c4b19855cf13f9de9f9a16a93cba
yura <triplef@disroot.org>
2023-12-11 11:57:27 +0600

    ok

---
+++ src/test/java/group/AddStudentGroupsTest.java
@@ -0,0 +1,39 @@
+package group;
+
+import org.junit.jupiter.api.Test;
+import ru_omsu_fctk_simpleserver.server.Reader;
+import ru_omsu_fctk_simpleserver.server.Server;
+import ru_omsu_fctk_simpleserver.server.Writer;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AddStudentGroupsTest {
+    @Test
+    void addStudentGroupsTest() throws Exception {
+        Server server = new Server();
+
+        Reader reader1 = new Reader("addStudentGroups::{\"name\":\"MMB-101\"}");
+        Writer writer1 = server.executeRequest(reader1);
+        Reader reader2 = new Reader("addStudentGroups::{\"name\":\"MMB-102\"}");
+        Writer writer2 = server.executeRequest(reader2);
+        Reader reader3 = new Reader("addStudentGroups::{\"name\":\"MMB-103\"}");
+        Writer writer3 = server.executeRequest(reader3);
+
+        assertEquals(writer1.toString(), "200::{\"id\":1}");
+        assertEquals(writer2.toString(), "200::{\"id\":2}");
+        assertEquals(writer3.toString(), "200::{\"id\":3}");
+    }
+
+    @Test
+    void validateTest() throws Exception {
+        Server server = new Server();
+
+        Reader reader1 = new Reader("addStudentGroups::{\"name\":\"\"}");
+        Writer writer1 = server.executeRequest(reader1);
+        Reader reader2 = new Reader("addStudentGroups::{\"name\":\"MMB-10119851981919851919198198195119519818981981981981819818951\"}");
+        Writer writer2 = server.executeRequest(reader2);
+
+        assertEquals(writer1.toString(), "400::6:\"Validation error\":[\"name: empty\"]");
+        assertEquals(writer2.toString(), "400::6:\"Validation error\":[\"name: big\"]");
+    }
+}
---
+++ src/test/java/group/GetStudentGroupByIdTest.java
@@ -0,0 +1,51 @@
+package group;
+import org.junit.jupiter.api.Test;
+import ru_omsu_fctk_simpleserver.server.Reader;
+import ru_omsu_fctk_simpleserver.server.Server;
+import ru_omsu_fctk_simpleserver.server.Writer;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GetStudentGroupByIdTest {
+    @Test
+    void getStudentGroupByIdTest() throws Exception {
+        Server server = new Server();
+
+        Reader reader1 = new Reader("addStudentGroups::{\"name\":\"MMB-101\"}");
+        server.executeRequest(reader1);
+        Reader reader2 = new Reader("addStudentGroups::{\"name\":\"MMB-102\"}");
+        server.executeRequest(reader2);
+        Reader reader3 = new Reader("addStudentGroups::{\"name\":\"MMB-103\"}");
+        server.executeRequest(reader3);
+
+        Reader reader10 = new Reader("getStudentGroupById::{\"id\":\"1\"}");
+        Writer writer10 = server.executeRequest(reader10);
+        Reader reader20 = new Reader("getStudentGroupById::{\"id\":\"2\"}");
+        Writer writer20 = server.executeRequest(reader20);
+        Reader reader30 = new Reader("getStudentGroupById::{\"id\":\"3\"}");
+        Writer writer30 = server.executeRequest(reader30);
+
+        assertEquals(writer10.toString(), "200::{\"name\":\"MMB-101\"}");
+        assertEquals(writer20.toString(), "200::{\"name\":\"MMB-102\"}");
+        assertEquals(writer30.toString(), "200::{\"name\":\"MMB-103\"}");
+    }
+
+    @Test
+    void validateTest() throws Exception {
+        Server server = new Server();
+
+        Reader reader1 = new Reader("getStudentGroupById::{\"id\":\"\"}");
+        Writer writer1 = server.executeRequest(reader1);
+
+        assertEquals(writer1.toString(), "400::6:\"Validation error\":[\"id: zero\"]");
+    }
+
+    @Test
+    void exceptionTest() throws Exception {
+        Server server = new Server();
+        Reader reader1 = new Reader("getStudentGroupById::{\"id\":\"5\"}");
+        Writer writer1 = server.executeRequest(reader1);
+
+        assertEquals(writer1.toString(), "422::6:\"Нет объекта с данным id\":[]");
+    }
+}
