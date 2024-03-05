8028cbd974502a56b7d843c8e1bbd60a5b9034e0
triplef-6 <triplef@disroot.org>
2024-03-05 19:02:40 +0600

    /

---
+++ src/main/java/ru_omsu_fctk_simpleserver/server/handlers/subject/DeleteSubjectHandler.java
@@ -0,0 +1,43 @@
+package ru_omsu_fctk_simpleserver.server.handlers.subject;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import ru_omsu_fctk_simpleserver.controllers.ComonResponse;
+import ru_omsu_fctk_simpleserver.controllers.ControllerSubject;
+import ru_omsu_fctk_simpleserver.controllers.ResponseEntity;
+import ru_omsu_fctk_simpleserver.controllers.group.ControllerGroup;
+import ru_omsu_fctk_simpleserver.exception.HandleException;
+import ru_omsu_fctk_simpleserver.request.subject.DeleteSubjectRequest;
+import ru_omsu_fctk_simpleserver.response.subject.DeleteSubjectResponse;
+
+public class DeleteSubjectHandler {
+    private ControllerSubject controllerSubject;
+
+    public DeleteSubjectHandler(ControllerSubject controllerSubject) {
+        this.controllerSubject = controllerSubject;
+    }
+
+    public String handel(String json) throws HandleException {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            DeleteSubjectRequest request = mapper.readValue(json, DeleteSubjectRequest.class);
+            ResponseEntity<ComonResponse<DeleteSubjectResponse>> res = controllerSubject.deleteSubject(request);
+
+            ComonResponse<DeleteSubjectResponse> comonResponse = res.getAnswer();
+            if (comonResponse.isOk()) {
+                return mapper.writeValueAsString(res.getStatus()) + "::";
+            } else {
+                return mapper.writeValueAsString(res.getStatus()) + "::"
+                        + mapper.writeValueAsString(comonResponse.getCodeError()) + ":"
+                        + mapper.writeValueAsString(comonResponse.getMessageError()) + ":"
+                        + mapper.writeValueAsString(comonResponse.getDetails());
+            }
+        } catch (Exception e) {
+            throw new HandleException(e.getMessage());
+        }
+    }
+    
+    
+    
+    
+    
+}
