03e482b605e9c0ccf0df51356a73954ef8e449cd
triplef-6 <triplef@disroot.org>
2024-03-05 18:31:10 +0600

    /

---
+++ src/main/java/ru_omsu_fctk_simpleserver/services/subject/AddSubjectService.java
@@ -0,0 +1,23 @@
+package ru_omsu_fctk_simpleserver.services.subject;
+
+import ru_omsu_fctk_simpleserver.essence.Group;
+import ru_omsu_fctk_simpleserver.essence.Subject;
+import ru_omsu_fctk_simpleserver.repositories.RepositoryGroup;
+import ru_omsu_fctk_simpleserver.repositories.RepositorySubject;
+import ru_omsu_fctk_simpleserver.request.group.AddStudentGroupsRequest;
+import ru_omsu_fctk_simpleserver.request.subject.AddSubjectRequest;
+import ru_omsu_fctk_simpleserver.response.group.AddStudentGroupsResponse;
+import ru_omsu_fctk_simpleserver.response.subject.AddSubjectsResponse;
+
+public class AddSubjectService {
+    private RepositorySubject repositorySubject;
+
+    public AddSubjectService(RepositorySubject repositorySubject) {
+        this.repositorySubject = repositorySubject;
+    }
+
+
+    public AddSubjectsResponse addStudentGroups(AddSubjectRequest request) {
+        return new AddSubjectsResponse(repositorySubject.addSubject(new Subject(request.getName(), request.getShortName())));
+    }
+}
