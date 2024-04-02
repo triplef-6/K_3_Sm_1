3291f42de020e896c8946a1024a9af34b7c9a01a
triplef-6 <triplef@disroot.org>
2024-04-02 17:44:11 +0600

    +

--- src/main/java/ru_omsu_fctk_simpleserver/controllers/ControllerAudience.java
+++ src/main/java/ru_omsu_fctk_simpleserver/controllers/ControllerAudience.java
@@ -1,14 +1,18 @@
 package ru_omsu_fctk_simpleserver.controllers;
 
 import ru_omsu_fctk_simpleserver.request.audience.AddAudienceRequest;
+import ru_omsu_fctk_simpleserver.request.audience.GetAudiencesBuildingRequest;
 import ru_omsu_fctk_simpleserver.request.audience.GetAudiencesRequest;
 import ru_omsu_fctk_simpleserver.request.group.AddStudentGroupsRequest;
 import ru_omsu_fctk_simpleserver.response.audience.AddAudienceResponse;
+import ru_omsu_fctk_simpleserver.response.audience.GetAudiencesBuildingResponse;
 import ru_omsu_fctk_simpleserver.response.audience.GetAudiencesResponse;
 import ru_omsu_fctk_simpleserver.response.group.AddStudentGroupsResponse;
 import ru_omsu_fctk_simpleserver.services.audience.AddAudienceService;
+import ru_omsu_fctk_simpleserver.services.audience.GetAudiencesBuildingService;
 import ru_omsu_fctk_simpleserver.services.audience.GetAudiencesService;
 import ru_omsu_fctk_simpleserver.validator.audience.AddAudienceValidator;
+import ru_omsu_fctk_simpleserver.validator.audience.GetAudiencesBuildingValidator;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,12 +20,16 @@ import java.util.List;
 public class ControllerAudience {
     private final AddAudienceService addAudiencesService;
     private final GetAudiencesService getAudiencesService;
+    private final GetAudiencesBuildingService getAudiencesBuildingService;
     private final AddAudienceValidator addAudiencesValidator;
+    private final GetAudiencesBuildingValidator getAudiencesBuildingValidator;
 
-    public ControllerAudience(AddAudienceService addStudentGroupsService, GetAudiencesService getStudentGroupsService, AddAudienceValidator addStudentGroupsValidator) {
+    public ControllerAudience(AddAudienceService addStudentGroupsService, GetAudiencesService getStudentGroupsService, AddAudienceValidator addStudentGroupsValidator, GetAudiencesBuildingValidator getAudiencesBuildingValidator, GetAudiencesBuildingService getAudiencesBuildingService) {
         this.addAudiencesService = addStudentGroupsService;
         this.getAudiencesService = getStudentGroupsService;
         this.addAudiencesValidator = addStudentGroupsValidator;
+        this.getAudiencesBuildingValidator = getAudiencesBuildingValidator;
+        this.getAudiencesBuildingService = getAudiencesBuildingService;
     }
     public ResponseEntity<ComonResponse<AddAudienceResponse>> addAudience(AddAudienceRequest request) {
         int status = 200;
@@ -57,7 +65,27 @@ public class ControllerAudience {
 
         return new ResponseEntity<>(comonResponse, status);
     }
+    public ResponseEntity<ComonResponse<GetAudiencesBuildingResponse>> getAudiencesByBuilding(GetAudiencesBuildingRequest request) {
+        int status = 200;
+        ComonResponse<GetAudiencesBuildingResponse> comonResponse;
 
+        List<String> errors = getAudiencesBuildingValidator.validator(request);
+
+        if (errors.isEmpty()) {
+            try {
+                comonResponse = new ComonResponse<>(getAudiencesBuildingService.getAudiencesBuilding(request));
+            } catch (Exception exception) {
+                status = 422;
+                comonResponse = new ComonResponse<>(6, exception.getMessage(), errors);
+            }
+        } else {
+            status = 400;
+            comonResponse = new ComonResponse<>(6, "Validation error", errors);
+            //comonResponse = new ComonResponse<>(6, "Error", errors);
+        }
+
+        return new ResponseEntity<>(comonResponse, status);
+    }
 
 
 
